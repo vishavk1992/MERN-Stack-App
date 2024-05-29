b1d4b735a5dbf0c1ecf12d610e6d5bbb17688498
vishavk1992 <vishavdeep551@gmail.com>
2024-05-29 15:50:01 -0400

    added admin menu page and modify admindasdboard

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const Spinner = ({ path = "login" }) => {
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(3);
   const navigate = useNavigate();
   const location = useLocation();
 
