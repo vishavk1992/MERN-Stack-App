6d957d592e5978223c45167cb7043abfd05d8a50
vishavk1992 <vishavdeep551@gmail.com>
2024-06-25 10:20:49 -0400

    "fix error"

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,12 +9,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // payment gateway
-var gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
+// var gateway = new braintree.BraintreeGateway({
+//   environment: braintree.Environment.Sandbox,
+//   merchantId: process.env.BRAINTREE_MERCHANT_ID,
+//   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+//   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+// });
 
 export const createProductController = async (req, resp) => {
   try {
